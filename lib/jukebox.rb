baab8106a2825074fd32a8c2767449cbddba1fcd
Paula Tulis <tulis.paula@gmail.com>
2019-04-16 19:27:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,7 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  answer = gets.chomp 
+  answer = gets.downcase.chomp 
   if answer == (1..9).to_s 
     puts "Playing #{songs[answer.to_i - 1]}"
     elsif answer == songs.include?(answer)
