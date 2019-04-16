d540e23eaa8a0146d1300c56ddfb76a24a6c485a
Paula Tulis <tulis.paula@gmail.com>
2019-04-16 19:19:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,32 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help 
+  puts"I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end 
+
+def list(songs)
+  songs.each.with_index do |song, index|
+  puts "#{index+ 1}. #{song}"
+end 
+end 
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  answer = gets.chomp 
+  if answer == songs || answer == songs.with_index[1]
+    puts "Playing #{answer}"
+  else puts "Invalid input, please try again"
+end 
+end 
+
+def exit_jukebox 
+  puts "Goodbye"
+end 
+
+def run 
+end 
